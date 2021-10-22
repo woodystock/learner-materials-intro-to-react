a772e3f3d5c3d3f58cbacb832fa3c643d61ba8d1
Ellie Judge <judge_ellie.com>
2021-10-22 19:44:19 +0100

    adds nav

--- src/App.js
+++ src/App.js
@@ -2,123 +2,17 @@ import './App.css';
 
 function App() {
 
-  const cats = [
-    {
-      "name": "Little Miss Purrfect",
-      "species": "Cat",
-      "favFoods": ["wet food", "dry food"],
-      "birthYear": 2016,
-      "photo": "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
-    },
-    {
-      "name": "Mr Gwumpy",
-      "species": "Cat",
-      "favFoods": ["caviar"],
-      "birthYear": 2016,
-      "photo": "https://cdn.cnn.com/cnnnext/dam/assets/190517103414-01-grumpy-cat-file-restricted.jpg"
-    },
-    {
-      "name": "Dopey",
-      "species": "Cat",
-      "favFoods": ["bugs"],
-      "birthYear": 2018,
-      "photo": "https://i1.wp.com/katzenworld.co.uk/wp-content/uploads/2019/06/funny-cat.jpeg?fit=1920%2C1920&ssl=1"
-    },
-    {
-      "name": "Jumpy",
-      "species": "Scaredy Cat",
-      "favFoods": ["not cucumbers!"],
-      "birthYear": 2008,
-      "photo": "https://i.ytimg.com/vi/eX2qFMC8cFo/maxresdefault.jpg"
-    },
-    {
-      "name": "Meowsalot",
-      "species": "Cat",
-      "favFoods": ["tuna", "catnip", "celery"],
-      "birthYear": 2012,
-      "photo": "https://learnwebcode.github.io/json-example/images/cat-1.jpg"
-    },
-    {
-      "name": "Cindy Clawford",
-      "species": "Cat",
-      "favFoods": ["mice"],
-      "birthYear": 2012,
-      "photo": "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
-    },
-    {
-      "name": "Katy Purry",
-      "species": "Cat",
-      "favFoods": ["cigarettes", "coffee"],
-      "birthYear": 2015,
-      "photo": "https://www.rd.com/wp-content/uploads/2021/03/GettyImages-1183822926.jpg"
-    },
-    {
-      "name": "Dr. Von Belly-Rub",
-      "species": "Cat",
-      "favFoods": ["salt"],
-      "birthYear": 2020,
-      "photo": "https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop"
-    },
-    {
-      "name": "Blobby",
-      "species": "Bird?",
-      "favFoods": ["your soul"],
-      "birthYear": 2020,
-      "photo": "https://www.top13.net/wp-content/uploads/2015/10/perfectly-timed-cat-photos-funny-cover.jpg"
-    },
-    {
-      "name": "El Taco",
-      "species": "Cat",
-      "favFoods": ["tequila", "bar snax"],
-      "birthYear": 1995,
-      "photo": "https://i.imgflip.com/mh1vd.jpg"
-    },
-    {
-      "name": "Nibbles",
-      "species": "Hungry Cat",
-      "favFoods": ["blankets", "feet", "tinsel"],
-      "birthYear": 2003,
-      "photo": "https://i.pinimg.com/736x/1b/ef/b5/1befb5723f54627768e1d77642ca4d45.jpg"
-    },
-    {
-      "name": "BoopySnoot",
-      "species": "Tiny Cat",
-      "favFoods": ["milk"],
-      "birthYear": 2021,
-      "photo": "https://i.ytimg.com/vi/ZKBlMWI6nO4/maxresdefault.jpg"
-    },
-    {
-      "name": "Tiger",
-      "species": "Fearsome Cat",
-      "favFoods": ["anything that moves", "anything else"],
-      "birthYear": 2020,
-      "photo": "https://static.onecms.io/wp-content/uploads/sites/47/2021/04/13/kitten-pounce-158906394-2000.jpg"
-    },
-    {
-      "name": "Smushi",
-      "species": "Sushi Cat",
-      "favFoods": ["ramen"],
-      "birthYear": 2000,
-      "photo": "https://www.mythirtyspot.com/wp-content/uploads/2014/09/Screen-Shot-2014-09-18-at-10.19.29-PM-1024x712.png"
-    },
-    {
-      "name": "Doughnut",
-      "species": "Party Cat",
-      "favFoods": ["mushrooms"],
-      "birthYear": 2014,
-      "photo": "https://rlv.zcache.co.uk/doughnut_cat_pet_cat_kitten_cat_cute_cats_postcard-rdef91d9c144140c5b6ac0b9516085db9_ucbjp_540.jpg"
-    },
-    {
-      "name": "Blep",
-      "species": "Cat",
-      "favFoods": ["baked bean juice"],
-      "birthYear": 2016,
-      "photo": "https://metro.co.uk/wp-content/uploads/2020/05/PRI_150473631-e1588316954238.jpg?quality=90&strip=all"
-    }
-
-  ];
   return (
     <>
+
+      <div className="nav">
+        <ul className="nav">
+          <li ><a className="nav-links" href="default.asp">Home</a></li>
+          <li><a className="nav-links" href="news.asp">News</a></li>
+          <li><a className="nav-links" href="contact.asp">Contact</a></li>
+          <li><a className="nav-links" href="about.asp">About</a></li>
+        </ul>
+      </div>
 
       <header className="header">
         <h1>Welcome to React!</h1>
@@ -126,7 +20,7 @@ function App() {
 
       <div className="card-container">
         <div className="card">
-          <h3 className="cat-name">Cindy Clawford</h3>
+          <h2 className="cat-name">Cindy Clawford</h2>
           <img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png" alt=""></img>
           <p className="species">Species: Cat</p>
           <p className="fav-foods">Favourite Food(s): Mice</p>
@@ -134,19 +28,27 @@ function App() {
         </div>
 
         <div className="card">
-          <h3 className="cat-name">Cindy Clawford</h3>
-          <img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png" alt=""></img>
+          <h2 className="cat-name">Little Miss Purrfect</h2>
+          <img src="https://learnwebcode.github.io/json-example/images/cat-2.jpg" alt=""></img>
           <p className="species">Species: Cat</p>
-          <p className="fav-foods">Favourite Food(s): Mice</p>
-          <p className="birth-year">Birth Year: 2012</p>
+          <p className="fav-foods">Favourite Food(s): Wet food, dry food</p>
+          <p className="birth-year">Birth Year: 2016</p>
         </div>
 
         <div className="card">
-          <h3 className="cat-name">Cindy Clawford</h3>
-          <img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png" alt=""></img>
+          <h2 className="cat-name">El Taco</h2>
+          <img src="https://i.imgflip.com/mh1vd.jpg" alt=""></img>
           <p className="species">Species: Cat</p>
-          <p className="fav-foods">Favourite Food(s): Mice</p>
-          <p className="birth-year">Birth Year: 2012</p>
+          <p className="fav-foods">Favourite Food(s): tequila, bar snax</p>
+          <p className="birth-year">Birth Year: 1995</p>
+        </div>
+
+        <div className="card">
+          <h2 className="cat-name">Mr Gwumpy</h2>
+          <img src="https://cdn.cnn.com/cnnnext/dam/assets/190517103414-01-grumpy-cat-file-restricted.jpg" alt=""></img>
+          <p className="species">Species: Cat</p>
+          <p className="fav-foods">Favourite Food(s): Caviar</p>
+          <p className="birth-year">Birth Year: 1984</p>
         </div>
       </div>
 
